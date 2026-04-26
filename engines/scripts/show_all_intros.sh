20e3931dea2a4c0ec4218dcdf103c74b5f4c29f1
RomeoCavazza <romeo.cavazza@gmail.com>
2026-04-26 02:19:30 +0200

    feat: final quality audit and PDF exports for 20 internship applications

--- engines/scripts/show_all_intros.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Script pour afficher toutes les intros (pitch) des resume.json
-find data/instances -name resume.json | while read file; do
-  intro=$(jq -r '.profile.pitch' "$file")
-  echo "${file}:"
-  echo "$intro"
-  echo "---"
-done
